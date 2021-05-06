ac9d5b8ebfe20c32c7f13f38bd26f262de7eabb1
dat <kdat036@gmail.com>
2021-05-06 14:34:03 +0700

    Initial Commit

---
+++ moviebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moviebase
+//
+//  Created by NguyenKhacDat on 06/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
